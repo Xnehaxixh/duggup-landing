e99e40af51929f06a3db5cc35c1c920a502003ca
Xnehaxixh <uchihaitachipik@gmail.com>
2024-03-11 22:33:39 +0530

    feat: add components

---
+++ src/lib/components/ui/card/card-image.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { HTMLImgAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js.js';
+
+	type $$Props = HTMLImgAttributes;
+
+	let className: $$Props['class'] = undefined;
+	let alt: $$Props['alt'] = undefined;
+	let src: $$Props['src'] = undefined;
+	export { className as class };
+</script>
+
+<img class={cn('h-auto w-full', className)} {src} {alt} {...$$restProps} />
---
+++ src/lib/components/ui/card/index.ts
@@ -0,0 +1,27 @@
+import Root from "./card.svelte";
+import Image from "./card-image.svelte";
+import Content from "./card-content.svelte";
+import Description from "./card-description.svelte";
+import Footer from "./card-footer.svelte";
+import Header from "./card-header.svelte";
+import Title from "./card-title.svelte";
+
+export {
+	Root,
+	Image,
+	Content,
+	Description,
+	Footer,
+	Header,
+	Title,
+	//
+	Root as Card,
+	Image as CardImage,
+	Content as CardContent,
+	Description as CardDescription,
+	Footer as CardFooter,
+	Header as CardHeader,
+	Title as CardTitle,
+};
+
+export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
